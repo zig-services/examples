9910918003c8c5e76c5d10baadeb6751ce215e22
Oliver Bestmann <obest@esailors.de>
2018-10-17 07:57:30 +0000

    Update zig-js library

---
+++ pack.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+# clean target directory
+rm -rf dist/zig-demo-game
+mkdir -p dist/zig-demo-game
+
+cp -ra src .gitignore *.html *.json README.md dist/zig-demo-game
+cd dist
+tar -c zig-demo-game | gzip -9 > zig-demo-game.tar.gz
+
