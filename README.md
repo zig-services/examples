0e34bfc6cab0b1dbdd8647d3a5efa80634942822
Oliver Bestmann <obest@esailors.de>
2018-10-17 07:54:45 +0000

    Update zig-js library

--- README.md
+++ README.md
@@ -1,6 +1,1 @@
 Install dependencies using `npm install` and run `npm run build` to build files in dist/ directory.
-
-
-Run a local http server, e.g using: `python3 -m http.server 8000`
-
-Open developer tools and navigate to `http://localhost:8000/parent.html`
--- src/index.js
+++ src/index.js
@@ -1,42 +1,49 @@
-import {Zig} from "zig-js/libzig";
+import {Zig} from "zig-js/zig/zig";
 
-// TODO: for debugging, remove in production
-localStorage.setItem("kv.logging", "true");
+window.onload = async () => {
+    // TODO: for debugging, remove in production
+    localStorage.setItem("kv.logging", "true");
 
-// log all messages during development:
-Zig.Client.Messages.messageClient.register(logEvent);
+    // wait for zig-js to be ready.
+    await Zig.ready();
 
-// wait for the player to start the game
-Zig.Client.Messages.registerGeneric({
-    async playGame() {
-        Game.runGame(Zig.Client.buyTicket());
-    },
+    // log all messages during development:
+    Zig.Client.Messages.messageClient.register(logEvent);
 
-    async playDemoGame() {
-        Game.runGame(Zig.Client.demoTicket())
-    },
-});
+    // wait for the player to start the game
+    Zig.Client.Messages.registerGeneric({
+        async playGame() {
+            Game.runGame(Zig.Client.buyTicket());
+        },
+
+        async playDemoGame() {
+            Game.runGame(Zig.Client.demoTicket())
+        },
+    });
+};
 
 const Game = {
     async runGame(ticket$) {
         // get the ticket
         const ticket = await ticket$;
 
-        // // you should handle errors, similar to this:
-        // try {
-        //     // await ...
-        // } catch(err) {
-        //     Zig.Client.Messages.error(err)
-        // }
-
-        // let the customer play the ticket
-        console.log("Got ticket from backend:", ticket);
+        // now handle the game.
+        // you should handle errors, similar to this:
+        try {
+            await Game.play(ticket)
+        } catch (err) {
+            Zig.Client.Messages.error(err)
+        }
 
         // settle the ticket.
         await Zig.Client.settleTicket(ticket.id);
 
         // tell the parent that the game has finish
         Zig.Client.Messages.gameFinished();
+    },
+
+    async play(ticket) {
+        console.log("Playing ticket:", ticket);
     },
 };
 
