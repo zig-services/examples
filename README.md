22bfb940d273d5ce7ea7e4f85b4da509e4804331
Oliver Bestmann <obest@esailors.de>
2018-10-19 09:21:29 +0000

    Update documentation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Install dependencies using `npm install` and run `npm run build` to build files in dist/ directory.
+# Demo Game Project
+
+**This is a demo project and just serves as an idea on how to setup a game project.**
+
+
+## Building and Testing
+Install dependencies using `npm install` and run `npm run build` to build files in `dist/` directory.
+
+To test your game on the debug page, edit the `debug.html` file to include a scenario compatible with your game, run a simple local http server in your games directory (e.g. `python3 -m http.server 2080`) and open the `debug.html` at http://localhost:2080/debug.html.
+
+Your browser will probably block the integration of your local game into the https enabled demo site. You need to allow that by clicking on `Load unsafe scripts` as seen in the following screenshot.
+
+![Screenshot](screenshot.png)
+
+## Zig Client
+The zig client is included as a dependency in the `package.json` as `zig-js`.
+For more information on the zig client, checkout the package documentation
+on [npm](https://www.npmjs.com/package/zig-js).
--- src/index.js
+++ src/index.js
@@ -7,20 +7,22 @@ window.onload = async () => {
     // wait for zig-js to be ready.
     await Zig.ready();
 
-    // log all messages during development:
+    // log all messages during development.
+    // TODO remove later!
     Zig.Client.Messages.messageClient.register(logEvent);
 
     // wait for the player to start the game
     Zig.Client.Messages.registerGeneric({
-        async playGame() {
+        playGame() {
             Game.runGame(Zig.Client.buyTicket());
         },
 
-        async playDemoGame() {
+        playDemoGame() {
             Game.runGame(Zig.Client.demoTicket())
         },
     });
 
+    // tell the parent frame to update
     Zig.Client.Messages.gameLoaded();
 };
 
@@ -35,6 +37,7 @@ const Game = {
             await Game.play(ticket)
         } catch (err) {
             Zig.Client.Messages.error(err)
+            return;
         }
 
         // settle the ticket.
@@ -46,11 +49,16 @@ const Game = {
 
     async play(ticket) {
         console.log("Playing ticket:", ticket);
+        console.log("Use the scenario to display your game: ", ticket.decodedScenario);
     },
 };
 
 function logEvent(event) {
     const div = document.createElement("div");
+    div.style.font = "monospace";
+    div.style.marginTop = "0.33em";
+    div.style.background = "#eee";
+    div.style.padding = "0.33em";
     div.innerText = JSON.stringify(event);
-    document.querySelector("#events").appendChild(div);
+    document.body.appendChild(div);
 }
